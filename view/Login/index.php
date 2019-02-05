f0719261172301a0871af80023b3db6796ceb828
jeffreydereus <99045621@mydavinci.nl>
2019-02-05 12:55:28 +0100

    Styled Login and register pages. Just needs Username and Password check.

--- view/Login/index.php
+++ view/Login/index.php
@@ -1,19 +1,50 @@
-<!----------- Container ------------>
-<div class="container" style="height: 80vh;">
-    <!----------- Context ------------>
-    <div class="col-md-12">
-        <form action="<?= URL ?>Login/LoginSession" method="POST" class="form">
-            <div class="imgcontainer">
+<div class="container">
+    <div id="loginbox" style="margin-top:50px;" class="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
+        <div class="panel panel-info" >
+            <div class="panel-heading">
+                <div class="panel-title">Sign In</div>
             </div>
-            <div class="container">
-                <label><b>Username</b></label>
-                <input type="text" placeholder="Enter Username" name="UsrName" class="form input" required>
-                <br>
-                <label><b>Password</b></label>
-                <input type="password" placeholder="Enter Password" name="UsrPass" class="form input" required>
-                <br>
-                <button type="submit" class="form input button" >Login</button> <br>
+
+            <div style="padding-top:30px" class="panel-body" >
+
+                <div style="display:none" id="login-alert" class="alert alert-danger col-sm-12"></div>
+
+                <form id="loginform" class="form-horizontal" role="form" method="post" action="<?= URL ?>Login/LoginSession">
+
+                    <div style="margin-bottom: 25px" class="input-group">
+                        <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+                        <input id="login-username" type="text" class="form-control" name="UsrName" value="" placeholder="username">
+                    </div>
+
+                    <div style="margin-bottom: 25px" class="input-group">
+                        <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
+                        <input id="login-password" type="password" class="form-control" name="UsrPass" placeholder="password">
+                    </div>
+
+                    <div style="margin-top:10px; margin-right:5px;" class="form-group">
+                        <!-- Button -->
+
+                        <div class="col-sm-12 controls">
+                            <button id="btn-login" type="submit" class="btn btn-success">Login</button>
+                        </div>
+                    </div>
+
+
+                    <div class="form-group">
+                        <div class="col-md-12 control">
+                            <div style="border-top: 1px solid#888; padding-top:15px; font-size:85%" >
+                                Don't have an account!
+                                <a href="<?=URL?>Login/register">
+                                    Sign Up Here
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+
+
+
             </div>
-        </form>
+        </div>
     </div>
 </div>
--- view/Login/register.php
+++ view/Login/register.php
@@ -1,7 +1,48 @@
-<h1>Registreer</h1>
+<!--<h1>Registreer</h1>-->
+<!---->
+<!--<form method="post" action="--><?//=URL?><!--Login/RegisterToDB">-->
+<!--    <p>Gebruikersnaam: <input type="text" name="UsrName"></p>-->
+<!--    <p>Wachtwoord: <input type="password" name="UsrPass"></p>-->
+<!--    <input type="submit" name="submit">-->
+<!--</form>-->
 
-<form method="post" action="<?=URL?>Login/RegisterToDB">
-    <p>Gebruikersnaam: <input type="text" name="UsrName"></p>
-    <p>Wachtwoord: <input type="password" name="UsrPass"></p>
-    <input type="submit" name="submit">
-</form>
+<div id="signupbox" style="margin-top:50px" class="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
+    <div class="panel panel-info">
+        <div class="panel-heading">
+            <div class="panel-title">Sign Up</div>
+            <div style="float:right; font-size: 85%; position: relative; top:-10px"><a id="signinlink" href="<?=URL?>Login/index">Sign In</a></div>
+        </div>
+        <div class="panel-body" >
+            <form id="signupform" class="form-horizontal" role="form" method="post" action="<?=URL?>Login/RegisterToDB">
+
+                <div id="signupalert" style="display:none" class="alert alert-danger">
+                    <p>Error:</p>
+                    <span></span>
+                </div>
+
+
+
+                <div class="form-group">
+                    <label for="UsrName" class="col-md-3 control-label">Gebruikersnaam</label>
+                    <div class="col-md-9">
+                        <input type="text" class="form-control" name="UsrName" placeholder="Gebruikersnaam">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="UsrPass" class="col-md-3 control-label">Wachtwoord</label>
+                    <div class="col-md-9">
+                        <input type="password" class="form-control" name="UsrPass" placeholder="Wachtwoord">
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <!-- Button -->
+                    <div class="col-md-offset-3 col-md-9">
+                        <button id="btn-signup" type="submit" class="btn btn-info"><i class="icon-hand-right"></i> &nbsp Sign Up</button>
+                    </div>
+                </div>
+
+            </form>
+        </div>
+    </div>
+</div>
